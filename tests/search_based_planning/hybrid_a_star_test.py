4cddd6454bd3428e734926c3373b14a3dad13d4d
SaulGoodmanCN <52434735+SoulGoodmanCN@users.noreply.github.com>
2024-09-12 02:08:34 +0000

    delete plot_arrow

--- tests/search_based_planning/hybrid_a_star_test.py
+++ tests/search_based_planning/hybrid_a_star_test.py
@@ -100,7 +100,6 @@ def plot_car(x, y, yaw):
         car_outline_y.append(converted_xy[1] + y)
 
     arrow_x, arrow_y, arrow_yaw = c * 1.5 + x, s * 1.5 + y, yaw
-    plot_arrow(arrow_x, arrow_y, arrow_yaw)
 
     plt.plot(car_outline_x, car_outline_y, car_color)
 
